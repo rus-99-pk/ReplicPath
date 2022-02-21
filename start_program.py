3e9cc067e17677a5c18be8895d5871212a663a37
rus-99-pk <ruslan.galeev.99@yandex.ru>
2022-02-21 23:58:57 +0300

    Update all files

---
+++ start_program.py
@@ -0,0 +1,55 @@
+# Импорт библиотек
+import os
+import datetime
+import time
+from example_my_os import ExampleMyOS
+from work import Work
+
+# Переменные для заполнения пустоты в классе "ExampleMyOS"
+#  в случае, если ОС - *nix.
+disk_source = ''
+disk_replic = ''
+disk_log = ''
+            
+#=====================================================
+
+# Если ОС - Windows, то будут запрошены имена Томов для директорий
+if (os.sys.platform == 'win32'):
+    my_os = 'Windows'
+    disk_source = input ('Диск, который будет использоваться для источника: ')
+    disk_replic = input ('Диск, который будет использоваться для реплики: ')
+    disk_log = input ('Диск, который будет использоваться для файла логирования: ')
+
+#  Заполнение списка пулом путей к директориям
+paths = []
+for circle in range(1, 4):
+    directory = ExampleMyOS(circle, disk_source, disk_replic, disk_log)
+    directory.user_source(circle)
+    paths.append(directory.my_path)
+
+# 0 - Директория-источник, 1 - Директория-реплика, 2 - Директория логов.
+source_dir = paths[0]
+replic_dir = paths[1]
+log_dir = paths[2]
+
+#=====================================================
+
+# Запрос периодичности бэкапов и
+#  дальнейшее преобразование единиц времени.
+backup_delay = input('С какой периодичностью выполнять бэкапы?\nПримеры: 10s/10m/10c\n')
+backup_value = backup_delay[-1]
+backup_delay = int(backup_delay[:-1])
+os.system(directory.cls)
+if (backup_value == 'm'):
+    backup_delay = int(backup_delay) * 60
+if (backup_value == 'c'):
+    backup_delay = int(backup_delay) * 60**2
+
+# Выполнение бэкапов и вывод логов
+filework = Work(source_dir, replic_dir, log_dir, directory.slash, backup_delay)
+filework.print_create()
+active = True
+while active:
+    filework.timework()
+    filework.scan_folders()
+    time.sleep(backup_delay)
--- work.py
+++ work.py
@@ -1,97 +1,6 @@
-# Импорт библиотек
 import os
 import datetime
 import time
-
-# Переменные для заполнения пустоты в классе "ExampleMyOS"
-#  в случае, если ОС - *nix.
-disk_source = ''
-disk_replic = ''
-disk_log = ''
-
-class ExampleMyOS():
-    """Для создания экземпляров ОС пользователя и дальнейшей обработки"""
-    def __init__(self, path_circle, disk_source='', disk_replic='', disk_log=''):
-        """Инициализация ОС пользователя, чтобы персонализировать
-           команды оболочки.
-        """
-        # Инициализация path_circle, чтобы программа понимала
-        #  какие команды оболочки использовать.
-        self.path_circle = path_circle
-        if (os.sys.platform == 'win32'):
-            self.slash = chr(92)
-            self.ls_user_os = 'dir'
-            self.disk_source = disk_source + ':' + chr(92)
-            self.default_path = self.disk_source
-            self.disk_replic = disk_replic + ':' + chr(92)
-            self.disk_log = disk_log + ':' + chr(92)
-            self.cls = 'cls'
-        else:
-            self.slash = '/'
-            self.ls_user_os = 'ls'
-            self.default_path = '/'
-            self.cls = 'clear'
-            
-    def user_source(self, path_circle=''):
-        """
-        Здесь производится работа с пользовательскими директориями.
-         Запрашиваются пути к каждой из директорий, выбор
-         директории зависит от номера выполняемого цикла в теле
-         программы.
-        """
-        
-        # Выбор директории
-        self.path_circle = path_circle
-        if (self.path_circle == 1):
-            print_source = 'источника'
-        if (self.path_circle == 2):
-            print_source = 'реплики'
-        if (self.path_circle == 3):
-            print_source = 'логов'
-            
-        # Выбор пути к присвоенным именам директорий
-        os.system(self.cls)
-        if (os.sys.platform == 'win32'):
-            if (self.path_circle == 1):
-                path = self.disk_source
-            if (self.path_circle == 2):
-                path = self.disk_replic
-            if (self.path_circle == 3):
-                path = self.disk_log
-        else:
-            path = self.default_path
-
-        # Работа с пользовательским интерфейсом выбора директории
-        prev_path = ''
-        circle = 1
-        active = True
-        while active:
-            os.system(self.cls)
-            print ('\t===========================')
-            print ('\tВыберите директорию для ' + print_source + '.')
-            print ('\t===========================')
-            os.system('cd ' + path)
-            os.system(self.ls_user_os + ' ' + path)
-            print ('\n ПРИМЕЧАНИЕ: Если вы закончили ввод, нажмите "Q"')
-            print ('Чтобы вернуться в начальную директорию, введите ".."')
-            print ('\nКаталог ' + print_source + ': ' + path)
-
-            user_path = input (': ')
-            if (user_path == 'q') or (user_path == 'Q'):
-                active = False
-            elif (user_path == '..'):
-                path = self.default_path
-                circle = 1
-            elif (circle == 1):
-                path += user_path
-                circle += 1
-            elif (circle != 1):
-                path += self.slash + user_path
-            path = path.replace ('//', '/')
-            path = path.replace ('C:\C:', 'C:')
-            os.system(self.cls)
-            self.my_path = path
-        
 class Work():
     """Класс для работы с выводами логов на экран и в файл"""
     def __init__(self, source_dir, replic_dir, log_dir, slash, backup_delay):
@@ -162,7 +71,7 @@ class Work():
         print (message)
 
         # Вывод информации о периодичности создания логов (в теле программы вопрос к пользователю)
-        message = self.logtime + '    Репликация будет выполняться с периодичностью в ' + str(backup_delay) + ' секунд.\n'
+        message = self.logtime + '    Репликация будет выполняться с периодичностью в ' + str(self. backup_delay) + ' секунд.\n'
         self.logfile.write(message)
         print (message)
 
@@ -226,46 +135,3 @@ class Work():
                     os.system('cp -rf ' + '"' + self.source_dir + self.slash + source_object + '" "' + self.replic_dir + '"')
 
         print ('\nДля завершения работы программы нажмите "CTRL+C"\n')
-            
-#=====================================================
-
-# Если ОС - Windows, то будут запрошены имена Томов для директорий
-if (os.sys.platform == 'win32'):
-    my_os = 'Windows'
-    disk_source = input ('Диск, который будет использоваться для источника: ')
-    disk_replic = input ('Диск, который будет использоваться для реплики: ')
-    disk_log = input ('Диск, который будет использоваться для файла логирования: ')
-
-#  Заполнение списка пулом путей к директориям
-paths = []
-for circle in range(1, 4):
-    directory = ExampleMyOS(circle, disk_source, disk_replic, disk_log)
-    directory.user_source(circle)
-    paths.append(directory.my_path)
-
-# 0 - Директория-источник, 1 - Директория-реплика, 2 - Директория логов.
-source_dir = paths[0]
-replic_dir = paths[1]
-log_dir = paths[2]
-
-#=====================================================
-
-# Запрос периодичности бэкапов и
-#  дальнейшее преобразование единиц времени.
-backup_delay = input('С какой периодичностью выполнять бэкапы?\nПримеры: 10s/10m/10c\n')
-backup_value = backup_delay[-1]
-backup_delay = int(backup_delay[:-1])
-os.system(directory.cls)
-if (backup_value == 'm'):
-    backup_delay = int(backup_delay) * 60
-if (backup_value == 'c'):
-    backup_delay = int(backup_delay) * 60**2
-
-# Выполнение бэкапов и вывод логов
-filework = Work(source_dir, replic_dir, log_dir, directory.slash, backup_delay)
-filework.print_create()
-active = True
-while active:
-    filework.timework()
-    filework.scan_folders()
-    time.sleep(backup_delay)
